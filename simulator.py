76d2da9265f1220edc6367f5a85b62c74668460f
Rares Cristian <raresc2105@gmail.com>
2017-07-15 18:16:52 -0400

    use sample particles

--- simulator.py
+++ simulator.py
@@ -1,14 +1,15 @@
 import plot
 import particle
 import electricField
+import sampleParticles
 import matplotlib.pyplot as plt
 
 # Create 'size' of plot
 sz = 10
-plt.axis((-sz, sz, -	sz, sz))
+plt.axis((-sz, sz, -sz, sz))
 plt.ion()
 
-particles = [particle.Particle(1,1,1)]
+particles = sampleParticles.particle_set[5]
 
 ef = electricField.Electric_Field(particles)
 plot.draw(plt, ef)
