6685fdba7b66fc70c650e3ed26045bafdc1e41ff
Rares Cristian <raresc2105@gmail.com>
2017-07-15 17:57:55 -0400

    Electric Field class

---
+++ electricField.py
@@ -0,0 +1,40 @@
+import physics
+from particle import *
+
+class Electric_Field (object) :
+    
+    def __init__ (self, objs) :
+        self.particle = objs
+                    
+    def force_vec (self, x, y) :
+        Ex = 0.0
+        Ey = 0.0
+
+        for ob in self.particle :
+            test = Particle(x,y, 0)
+            d = physics.distance(ob, test)
+            if d <= 0.01 : 
+                return (0, 0, True)
+            else :
+                (ex, ey) = electricField(ob, test)
+
+                Ex = Ex + ex
+                Ey = Ey + ey
+
+        return (Ex, Ey, False)
+
+def electricField(particle, test) :
+	r = physics.distance(particle, test)
+	magnitude = particle.charge / (r * r)
+	
+	# split into components
+	ex = magnitude * (test.x - particle.x) / r
+	ey = magnitude * (test.y - particle.y) / r
+		
+	return (ex, ey)
+
+
+
+
+
+
