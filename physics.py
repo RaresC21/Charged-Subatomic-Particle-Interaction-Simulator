5e1bb28732d05c5cd7d0c600cf6814e189c07009
Rares Cristian <raresc2105@gmail.com>
2017-07-30 20:11:03 -0400

    implement forces between particles, and their movements

--- physics.py
+++ physics.py
@@ -6,3 +6,33 @@ def distance(p1, p2) :
     return np.sqrt(dx * dx + dy * dy)
     
 
+def simulate (particles, ef, lmbda, iterations, step_size) :    
+
+    k = 0
+    while (k < iterations) :
+    
+        for i in range(len(particles)) :
+
+            step(particles[i], lmbda)
+            if (k % step_size == 0) :
+                particles[i].xs.append(particles[i].x)
+                particles[i].ys.append(particles[i].y)           
+            
+            update(particles[i], ef, lmbda)
+        k += 1
+
+def step(particle, lmbda) :
+    particle.x += particle.vx * lmbda +  particle.ax * (lmbda ** 2)
+    particle.y += particle.vy * lmbda +  particle.ay * (lmbda ** 2)
+
+def update(particle, ef, lmbda):
+    Ex, Ey, s = ef.force_vec(particle.x, particle.y) 
+    Fx = Ex * particle.charge
+    Fy = Ey * particle.charge
+            
+    particle.vx += particle.ax * lmbda
+    particle.vy += particle.ay * lmbda       
+            
+    particle.ax = Fx / particle.mass
+    particle.ay = Fy / particle.mass
+    return particle
